f520df77dcd8edeaa56e449d4060f13b8983f552
rajendrachavan <rajendrachavan.jvm@gmail.com>
2019-09-18 13:41:51 +0530

    Created vet Speciality and associated it to Vet Class. closes #28

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -14,7 +14,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"/owners","/owners/index","/owners/index.html"})
+    @RequestMapping({"/owners","/owners/index","/owners/index.html","/owners.html"})
     public String listOwner(Model model){
         model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
